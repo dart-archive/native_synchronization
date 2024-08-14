6d8445565d8964efe636633d3eb204495772f841
Brett Sutton <bsutton@noojee.com.au>
2024-08-14 14:30:04 +1000

    changed /// to // as there comments are just to help with visual parsing.

--- lib/src/bindings/pthread.dart
+++ lib/src/bindings/pthread.dart
@@ -18,51 +18,51 @@ final class pthread_timespec_t extends Struct {
   external int tv_nsec;
 }
 
-/// mutex_init
+// mutex_init
 @Native<Int Function(Pointer<pthread_mutex_t>, Pointer<Void>)>()
 external int pthread_mutex_init(
     Pointer<pthread_mutex_t> mutex, Pointer<Void> attrs);
 
-/// mutex_lock
+// mutex_lock
 @Native<Int Function(Pointer<pthread_mutex_t>)>()
 external int pthread_mutex_lock(Pointer<pthread_mutex_t> mutex);
 
-/// mutex_timedlock
+// mutex_timedlock
 @Native<
     Int Function(
         Pointer<pthread_mutex_t>, Pointer<pthread_timespec_t> abstime)>()
 external int pthread_mutex_timedlock(
     Pointer<pthread_mutex_t> mutex, Pointer<pthread_timespec_t> abstime);
 
-/// mutex_unlock
+// mutex_unlock
 @Native<Int Function(Pointer<pthread_mutex_t>)>()
 external int pthread_mutex_unlock(Pointer<pthread_mutex_t> mutex);
 
-/// mutex_destroy
+// mutex_destroy
 @Native<Int Function(Pointer<pthread_mutex_t>)>()
 external int pthread_mutex_destroy(Pointer<pthread_mutex_t> cond);
 
-/// cond_init
+// cond_init
 @Native<Int Function(Pointer<pthread_cond_t>, Pointer<Void>)>()
 external int pthread_cond_init(
     Pointer<pthread_cond_t> cond, Pointer<Void> attrs);
 
-/// cond_wait
+// cond_wait
 @Native<Int Function(Pointer<pthread_cond_t>, Pointer<pthread_mutex_t>)>()
 external int pthread_cond_wait(
     Pointer<pthread_cond_t> cond, Pointer<pthread_mutex_t> mutex);
 
-/// cond_timedwait
+// cond_timedwait
 @Native<
     Int Function(Pointer<pthread_cond_t>, Pointer<pthread_mutex_t>,
         Pointer<pthread_timespec_t> abstime)>()
 external int pthread_cond_timedwait(Pointer<pthread_cond_t> cond,
     Pointer<pthread_mutex_t> mutex, Pointer<pthread_timespec_t> abstime);
 
-/// cond_destroy
+// cond_destroy
 @Native<Int Function(Pointer<pthread_cond_t>)>()
 external int pthread_cond_destroy(Pointer<pthread_cond_t> cond);
 
-/// cond_signal
+// cond_signal
 @Native<Int Function(Pointer<pthread_cond_t>)>()
 external int pthread_cond_signal(Pointer<pthread_cond_t> cond);
